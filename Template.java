90c2d6995a5268c79b9c21e38eed7ed354494a03
SairamCharanX <sairam.v99@gmail.com>
2023-01-29 23:37:53 +0530

    Revert "midNight"
    This reverts commit b3d4502abbe969c003e9151eed80e66ad00f9dcf.

--- Template.java
+++ Template.java
@@ -4,18 +4,15 @@ public class Template {
     public static void main(String[] args) {
 
 
-//        Scanner s = new Scanner(System.in);
-//        int cases = s.nextInt();
-//
-//        for (int index = 0;index < cases;index++){
-//
-//        }
+        Scanner s = new Scanner(System.in);
+        int cases = s.nextInt();
+
+        for (int index = 0;index < cases;index++){
+
+        }
 
 
         //TODO: macro for sout
-
-
-
 
     }
 }
