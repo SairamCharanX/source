eb2ce09618cf10557263bfc0d13b55cc22510fa7
sairamcharanx <sairam.v99@gmail.com>
2023-02-06 20:13:22 +0530

    starters71

---
+++ codechef/Starters71/Instagram.java
@@ -0,0 +1,27 @@
+package codechef.Starters71;
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Instagram
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner s = new Scanner(System.in);
+        int cases = s.nextInt();
+
+        for (int index = 0;index < cases;index++){
+            int following = s.nextInt();
+            int followers = s.nextInt();
+
+            if(following > (followers*10))
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+    }
+}
