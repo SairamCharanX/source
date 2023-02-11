21b7e1a216931d8e24096f14d59ceebc71e5c72b
sairamcharanx <sairam.v99@gmail.com>
2023-02-11 22:22:53 +0530

    general

---
+++ codechef/JanmanshAndAssignments.java
@@ -0,0 +1,25 @@
+package codechef;
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class JanmanshAndAssignments
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner s = new Scanner(System.in);
+        int cases = s.nextInt();
+
+        for (int index = 0;index < cases;index++){
+            int x = s.nextInt();
+            if(x+3<=10)
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+    }
+}
