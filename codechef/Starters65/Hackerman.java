7e90a1a85a29e5cc0487edd316f5d043c8940ab2
sairamcharanx <sairam.v99@gmail.com>
2023-02-11 21:49:34 +0530

    Commented out all code due to external library usage

--- codechef/Starters65/Hackerman.java
+++ codechef/Starters65/Hackerman.java
@@ -1,14 +1,12 @@
 package codechef.Starters65;
 
-/* package codechef; // don't place package name! */
-
+/*
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 import com.google.common.math.IntMath;
 import java.math.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
 class Hackerman
 {
     public static void main (String[] args) throws java.lang.Exception
@@ -27,3 +25,4 @@ class Hackerman
         }
     }
 }
+*/
