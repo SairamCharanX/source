2adc3934ff9172e0b83781eed9ce221716b0c068
sairamcharanx <sairam.v99@gmail.com>
2023-02-22 00:00:51 +0530

    restart

--- FileReaderExample.java
+++ FileReaderExample.java
@@ -1,7 +1,6 @@
 import java.io.FileReader;  
 public class FileReaderExample {  
     public static void main(String args[])throws Exception{
-        //change path accordingly
           FileReader fr=new FileReader("E:\\ApplicationFiles\\inputFile.txt");    
           int i;    
           while((i=fr.read())!=-1)    
