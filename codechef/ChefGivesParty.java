a48bdbfed36e673abbac27cf4f210eefaf4c6564
sairamcharanx <sairam.v99@gmail.com>
2023-03-04 22:08:42 +0530

    math

---
+++ codechef/ChefGivesParty.java
@@ -0,0 +1,28 @@
+package codechef;
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class ChefGivesParty
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner s = new Scanner(System.in);
+        int cases = s.nextInt();
+
+        for (int index = 0;index < cases;index++){
+            int n = s.nextInt();
+            int x = s.nextInt();
+            int k = s.nextInt();
+
+            if(k>=(n*x))
+                System.out.println("yes");
+            else
+                System.out.println("no");
+        }
+    }
+}
