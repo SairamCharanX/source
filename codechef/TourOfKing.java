d69d4333a631e0a164d313a4f5d5683838ba5e9a
sairamcharanx <sairam.v99@gmail.com>
2023-03-05 15:49:04 +0530

    modified TourOfKing

--- codechef/TourOfKing.java
+++ codechef/TourOfKing.java
@@ -1,13 +1,9 @@
 package codechef;
-
-public class TourOfKing {
-    /* package codechef; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-    /* Name of the class has to be "Main" only if the class is public. */
+public class TourOfKing {
 
         public static void main (String[] args) throws java.lang.Exception
         {
