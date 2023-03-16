47d446c1d439af0e2a03071c240a1d435cfc7b8f
sairamcharan <78950391+SairamCharanX@users.noreply.github.com>
2023-03-17 00:13:23 +0530

    PassTheExam

---
+++ codechef/PassTheExam.java
@@ -0,0 +1,31 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+		int cases = s.nextInt();
+
+		for(int index=0;index<cases;index++){
+		    int a = s.nextInt();
+		    int b = s.nextInt();
+		    int c = s.nextInt();
+
+		    if(a>=10 && b>=10 && c>=10){
+		        if(a+b+c>=100){
+		            System.out.println("pass");
+		        }
+		        else
+		        System.out.println("fail");
+		    }
+		    else
+		    System.out.println("fail");
+		}
+	}
+}
