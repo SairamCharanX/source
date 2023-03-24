8c36bd158f7efcd88059d4a5268f8554ccc52342
sairamcharan <78950391+SairamCharanX@users.noreply.github.com>
2023-03-24 23:36:49 +0530

    Create EnoughSpace.java

---
+++ EnoughSpace.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class EnoughSpace
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+        int cases = s.nextInt();
+
+        for (int index = 0;index < cases;index++){
+        int n = s.nextInt();
+        int x = s.nextInt();
+        int y = s.nextInt();
+        
+        if((x + 2*y)<=n)
+        System.out.println("yes");
+        else
+        System.out.println("no");
+        }
+	}
+}
