cfa8b2b172bb7d7bb396bdd0041210cd80226058
sairamcharanx <sairam.v99@gmail.com>
2023-03-24 22:49:13 +0530

    general

---
+++ codechef/ChessRatings.java
@@ -0,0 +1,30 @@
+package codechef;
+
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class ChessRatings
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner s = new Scanner(System.in);
+        int cases = s.nextInt();
+
+        for (int index = 0;index < cases;index++){
+            int x = s.nextInt();
+            int y = s.nextInt();
+            int games = 0;
+
+            while(x<y){
+                x+=8;
+                games+=1;
+            }
+            System.out.println(games);
+        }
+    }
+}
