e2a6442b69a8a06e26508e9cd570942820516aca
sairamcharan <78950391+SairamCharanX@users.noreply.github.com>
2023-03-24 23:32:42 +0530

    Create ChairsRequirement.java

---
+++ ChairsRequirement.java
@@ -0,0 +1,24 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class ChairsRequirement
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+        int cases = s.nextInt();
+
+        for (int index = 0;index < cases;index++){
+            int x = s.nextInt();
+            int y = s.nextInt();
+            if(y<=x)
+            System.out.println(Math.abs(x-y));
+            else
+            System.out.println("0");
+        }
+	}
+}
