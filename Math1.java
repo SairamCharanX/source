81dfbdd0be9d7d912152243b48a84c95d3bccbf6
sairamcharan <78950391+SairamCharanX@users.noreply.github.com>
2023-03-24 23:49:31 +0530

    Create Math1.java

---
+++ Math1.java
@@ -0,0 +1,25 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Math1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+        int cases = s.nextInt();
+
+        for (int index = 0;index < cases;index++){
+            int available = s.nextInt();
+            int required = s.nextInt();
+            
+            if(available>=required)
+            System.out.println("0");
+            else
+            System.out.println(Math.abs(available-required));
+        }
+	}
+}
