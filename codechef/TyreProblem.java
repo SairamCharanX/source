2a8e4d53317ee848c53eaea872051ae36722334c
sairamcharanx <sairam.v99@gmail.com>
2023-03-26 23:27:09 +0530

    casual

---
+++ codechef/TyreProblem.java
@@ -0,0 +1,23 @@
+package codechef;
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class TyreProblem
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner s = new Scanner(System.in);
+        int cases = s.nextInt();
+
+        for (int index = 0;index < cases;index++){
+            int bikes = s.nextInt();
+            int cars = s.nextInt();
+            System.out.println(2*bikes + 4*cars);
+        }
+    }
+}
