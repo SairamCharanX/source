14134cfafd8ea120032e3911b11d1d93f82794ea
sairamcharanx <sairam.v99@gmail.com>
2023-03-26 23:58:35 +0530

    casual

---
+++ codechef/CreditScore.java
@@ -0,0 +1,21 @@
+package codechef;
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class CreditScore
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        if(x>=750)
+            System.out.println("yes");
+        else
+            System.out.println("no");
+    }
+}
