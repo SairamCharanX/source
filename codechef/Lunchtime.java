1eb32c4e629014aaf897e3f33bd802f3d790a774
sairamcharanx <sairam.v99@gmail.com>
2023-03-27 23:48:12 +0530

    casual

---
+++ codechef/Lunchtime.java
@@ -0,0 +1,27 @@
+package codechef;
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Lunchtime
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner s = new Scanner(System.in);
+        int cases = s.nextInt();
+
+        for (int index = 0;index < cases;index++){
+            int x = s.nextInt();
+            if(x>=1 && x<=4){
+                System.out.println("yes");
+            }
+            else{
+                System.out.println("no");
+            }
+        }
+    }
+}
