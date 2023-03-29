ea263e54ef022c60c227c1c17b90f65ee689900e
sairamcharanx <sairam.v99@gmail.com>
2023-03-29 22:47:06 +0530

    starters83

---
+++ codechef/starters83/Technex.java
@@ -0,0 +1,24 @@
+package codechef.starters83;
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Technex
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner s = new Scanner(System.in);
+        int cases = s.nextInt();
+
+        for (int index = 0;index < cases;index++){
+            int r = s.nextInt();
+            r = r * 1000;
+            int remaining = r/2;
+            System.out.println(remaining/5);
+        }
+    }
+}
