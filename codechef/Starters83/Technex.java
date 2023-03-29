ceb6b17970fed598bd4d96e1e41f9e264847aaef
sairamcharanx <sairam.v99@gmail.com>
2023-03-29 23:27:11 +0530

    Refactoring

--- codechef/Starters83/Technex.java
+++ codechef/Starters83/Technex.java
@@ -1,10 +1,9 @@
-package codechef.starters83;
+package codechef.Starters83;
 
 /* package codechef; // don't place package name! */
 
 import java.util.*;
 import java.lang.*;
-import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
 class Technex
