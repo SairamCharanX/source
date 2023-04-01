9aead212f5098b7f82c5bb923be4e71a3c467228
sairamcharan <78950391+SairamCharanX@users.noreply.github.com>
2023-04-01 23:10:58 +0530

    Create TicketFine.java

---
+++ codechef/TicketFine.java
@@ -0,0 +1,23 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+        int cases = s.nextInt();
+
+        for (int index = 0;index < cases;index++){
+            int x = s.nextInt();
+            int p = s.nextInt();
+            int q = s.nextInt();
+            
+            System.out.println(Math.abs(p-q) * x);
+        }
+	}
+}
