850f94eec6e0c7a8004a3f60837d6d9562dddcf6
sairamcharanx <sairam.v99@gmail.com>
2023-04-16 16:07:54 +0530

    SaleSeason

---
+++ codechef/SaleSeason.java
@@ -0,0 +1,32 @@
+package codechef;
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class SaleSeason
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner s = new Scanner(System.in);
+        int cases = s.nextInt();
+
+        for (int index = 0;index < cases;index++){
+            int x = s.nextInt();
+            int discount = 0;
+            if(x<=100)
+                discount = 0;
+            else if (x>100 && x<=1000)
+                discount = 25;
+            else if(x>1000 && x<=5000)
+                discount = 100;
+            else
+                discount = 500;
+
+            System.out.println(x-discount);
+        }
+    }
+}
