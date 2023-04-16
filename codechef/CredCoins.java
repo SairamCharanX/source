67871592f60a307d228d1abe5844609057c1cc40
sairamcharanx <sairam.v99@gmail.com>
2023-04-16 16:23:43 +0530

    CRED

---
+++ codechef/CredCoins.java
@@ -0,0 +1,25 @@
+package codechef;
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class CredCoins
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner s = new Scanner(System.in);
+        int cases = s.nextInt();
+
+        for (int index = 0;index < cases;index++){
+            int x = s.nextInt();
+            int y = s.nextInt();
+
+            int count = (int)(x*y/100);
+            System.out.println(count);
+        }
+    }
+}
