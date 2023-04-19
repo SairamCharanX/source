5a1faf48f3fa30502e7cf070ba6d0f1e661fb65d
sairamcharanx <sairam.v99@gmail.com>
2023-04-19 23:19:22 +0530

    starters86

---
+++ codechef/starters86/CodechefLearn.java
@@ -0,0 +1,20 @@
+package codechef.starters86;
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class CodechefLearn
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+
+        System.out.println(2*x);
+
+    }
+}
