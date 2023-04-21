3fac412b8b2f07ffb031f2400e4ba14d36d1d7c8
sairamcharanx <sairam.v99@gmail.com>
2023-04-21 23:20:09 +0530

    casual

---
+++ codechef/ValentineIsComing.java
@@ -0,0 +1,26 @@
+package codechef;
+
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class ValentineIsComing
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner s = new Scanner(System.in);
+        int cases = s.nextInt();
+
+        for (int index = 0;index < cases;index++){
+            int x = s.nextInt();
+            int y = s.nextInt();
+
+            System.out.println((int)(x/y));
+        }
+    }
+}
+
