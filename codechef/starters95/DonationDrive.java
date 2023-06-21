415b2ab3394f0389f0c84b3f90a620342a3e7738
sairamcharanx <sairam.v99@gmail.com>
2023-06-22 00:08:00 +0530

    starters95

---
+++ codechef/starters95/DonationDrive.java
@@ -0,0 +1,25 @@
+package codechef.starters95;
+
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class DonationDrive
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner s = new Scanner(System.in);
+        int cases = s.nextInt();
+
+        for (int index = 0;index < cases;index++){
+            int n = s.nextInt();
+            int x = s.nextInt();
+
+            System.out.println(Math.abs(n-x));
+        }
+    }
+}
