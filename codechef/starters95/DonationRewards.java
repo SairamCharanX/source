4ed6c263fc02ba324fad687d6bfa5423a3dde09e
sairamcharanx <sairam.v99@gmail.com>
2023-06-22 00:09:15 +0530

    starters95

---
+++ codechef/starters95/DonationRewards.java
@@ -0,0 +1,27 @@
+package codechef.starters95;
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class DonationRewards
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner s = new Scanner(System.in);
+        int cases = s.nextInt();
+
+        for (int index = 0;index < cases;index++){
+            int x = s.nextInt();
+            if(x<=3)
+                System.out.println("BRONZE");
+            else if (x>3 && x<=6)
+                System.out.println("SILVER");
+            else
+                System.out.println("GOLD");
+        }
+    }
+}
