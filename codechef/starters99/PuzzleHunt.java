375cbf198ed162ca801e7c65a2713a2a49e03259
sairamcharanx <sairam.v99@gmail.com>
2023-07-19 20:26:23 +0530

    starters99

---
+++ codechef/starters99/PuzzleHunt.java
@@ -0,0 +1,23 @@
+package codechef.starters99;
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class PuzzleHunt
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        
+        if(n>=6 && n<=8)
+        System.out.println("YES");
+        else
+        System.out.println("NO");
+	}
+}
+
