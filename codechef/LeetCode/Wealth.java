6b1f201d00ce9c20f69c740e3c1c461362e7710b
sairamcharanx <sairam.v99@gmail.com>
2023-08-07 23:54:16 +0530

    Wealth

---
+++ codechef/LeetCode/Wealth.java
@@ -0,0 +1,11 @@
+package LeetCode;
+
+import java.util.Scanner;
+
+public class Wealth {
+    Scanner s = new Scanner(System.in);
+    int cust = 0;
+    int accs = 0;
+
+    int customers = s.nextInt(accs)
+}
