def81e1f130905c86b865b73e012f6cd89604427
sairamcharanx <sairam.v99@gmail.com>
2023-08-07 23:55:17 +0530

    2000 - CodeChef

---
+++ codechef/TwoThousand.java
@@ -0,0 +1,17 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class TwoThousand
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+        int notes = s.nextInt();
+        
+        System.out.println((notes * 2000) / 500);
+	}
+}
