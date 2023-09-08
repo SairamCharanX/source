2e661581e608f22413a7d471daba71667a89f57d
sairamcharanx <sairam.v99@gmail.com>
2023-09-08 22:36:37 +0530

    SeptemberShort2023

---
+++ codechef/SeptemberShort2023/LuckySeven.java
@@ -0,0 +1,13 @@
+package SeptemberShort2023;
+
+import java.util.*;
+
+class LuckySeven
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        System.out.println(input.charAt(6));
+	}
+}
