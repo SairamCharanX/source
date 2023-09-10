7c891274d055be37fd8ad5a954ddb06787ff7fb3
sairamcharanx <sairam.v99@gmail.com>
2023-09-10 23:14:21 +0530

    AugustLong2023

---
+++ codechef/AugustLong2023/AllNewCodechef.java
@@ -0,0 +1,24 @@
+package AugustLong2023;
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class AllNewCodechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scanner = new Scanner(System.in);
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		
+		if(x>y)
+		System.out.println("New");
+		else if(x<y)
+		System.out.println("Old");
+		else
+		System.out.println("Same");
+	}
+}
+
