19d7df4466147568de712d0f14497d7c19405716
sairamcharanx <sairam.v99@gmail.com>
2023-10-18 23:33:44 +0530

    starters104

---
+++ codechef/starters104/DoubleRent.java
@@ -0,0 +1,15 @@
+package starters104;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class DoubleRent
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scanner = new Scanner(System.in);
+        int price = scanner.nextInt();
+        System.out.println(2 * price);
+	}
+}
