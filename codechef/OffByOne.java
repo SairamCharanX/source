08aabc73f6f16360aef838a9280737cbed37211b
SairamCharanX <sairam.v99@gmail.com>
2024-10-19 22:55:31 +0530

    OffByOne

---
+++ codechef/OffByOne.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class OffByOne
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(scanner.nextInt() + scanner.nextInt() + "1");
+	}
+}
